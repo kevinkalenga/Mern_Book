105ae46698dbae766ae9b69fb9c1d196ca7006a9
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-10 22:28:03 +0200

    Create user mongodb schema

---
+++ backend/model/userModel.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+
+
+const userSchema = new mongoose.Schema({
+   name: {
+     type: String,
+     required: true,
+   },
+   email: {
+     type: String,
+     required: true,
+     unique: true,
+   },
+   password: {
+     type: String,
+     required: true
+   },
+   isAdmin: {
+     type: Boolean,
+     required: true,
+     default: false
+   }
+}, {
+    timestamps: true
+})
+
+// this.password is the pwd which is store in the database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+// pre mean sthing we want to do before saving (ex: hashing the pwd before saving)
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
+
