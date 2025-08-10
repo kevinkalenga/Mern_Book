efea189ea8c86edbbfe749ce0644b5f7c476f46a
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-10 04:48:12 +0200

    Implement product controller Part 1

---
+++ backend/controllers/productController.js
@@ -0,0 +1,77 @@
+import asyncHandler from "../middleware/asyncHandler";
+import Product from "../model/productModel"; 
+
+// get all products (public)
+ const getProducts = asyncHandler(async(req, res) => {
+    const products = await Product.find({});
+    res.json(products)
+})
+
+// get single product (public)
+ const getProductById = asyncHandler(async(req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+      return res.json(product)
+    } else {
+        res.status(404);
+       throw new Error('Resource not found');
+    }
+})
+
+// Create product (admin private)
+const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'Sample name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description',
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+});
+
+// update product (admin private)
+
+const updateProduct = asyncHandler(async (req, res) => {
+    const { name, price, description, image, category, countInStock } =
+        req.body;
+
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.category = category;
+        product.countInStock = countInStock;
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } else {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+});
+
+// update product (admin private)
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        await Product.deleteOne({ _id: product._id });
+        res.status(200).json({ message: 'Product deleted successfully' });
+    } else {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+});
+
+
+export {getProducts, getProductById, createProduct, updateProduct, deleteProduct}
