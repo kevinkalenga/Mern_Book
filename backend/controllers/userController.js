ec36a0e3301d49eb6fa5cb0c7808f5f4db3bb5db
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-11 02:21:57 +0200

    User controller part 2

---
+++ backend/controllers/userController.js
@@ -0,0 +1,139 @@
+import asyncHandler from "../middleware/asyncHandler";
+import User from "../model/userModel";
+import generateToken from "../utils/generateToken";
+import validator from 'validator'
+
+// sign in user 
+
+const authUser = asyncHandler(async(req, res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email})
+
+    if(user && (await user.matchPassword(password))) {
+        generateToken(res, user._id)
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error("Wrong email or password")
+    }
+})
+
+// sign up user 
+const registerUser = asyncHandler(async(req, res) => {
+    const {name, email, password} = req.body 
+
+    if(!validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase:1,
+        minUppercase:1,
+        minNumbers:1,
+        minSymbols:1
+    })){
+        res.status(400)
+        throw new Error(
+            "password must be at least 8 long, contain at least, one lowercase, one uppercase, one number and one symbol"
+        )
+    }
+
+    const userExist = await User.findOne({email});
+
+    if(userExist) {
+        res.status(400)
+        throw new Error("user already exist")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    })
+
+    if(user) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({ message: 'Logged out successfully' });
+})
+
+// get user profile (private)
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+
+
+    } else {
+        res.status(404);
+        throw new Error('User not found')
+    }
+})
+
+// update user profile
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        if(req.body.email && !validator.isEmail(req.body.email)) {
+           res.status(400)
+           throw new Error('Invalide email')
+        }
+        if(req.body.password && !validator.isStrongPassword(req.body.password, {
+            minLength: 1,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+        })) {
+             res.status(400);
+             throw new Error("password must be at least 8 long, contain at least, one lowercase, one uppercase, one number and one symbol")
+        }
+
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updateUser = await user.save();
+
+        res.status(200).json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error("user not found")
+    }
+})
