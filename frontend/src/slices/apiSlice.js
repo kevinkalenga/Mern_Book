37bd58526bca30bbc5859d4c4216f17c227e2332
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-16 02:01:08 +0200

    Implement auth slice and user api slice

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 import { BASE_URL } from '../constants.js'
 
+
 const baseQuery = fetchBaseQuery({
     baseUrl:BASE_URL,
 })
