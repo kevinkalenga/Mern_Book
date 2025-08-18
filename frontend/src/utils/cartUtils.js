42166bf4fd35fdc9cfd81b86f011d3c25fb136df
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-19 00:03:42 +0200

    Implement cart slice and cart utils functionality

---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,20 @@
+export const addDecimal = (num) => {
+   return (Math.round(num*100)/100).toFixed(2)
+}
+
+export const updateCart = (state) => {
+ const itemsPrice = state.cartItems.reduce((acc, item) => acc + (item.price * 100 * item.qty)/100, 0)
+ state.itemsPrice = addDecimal(itemsPrice)
+
+ const shippingPrice = itemsPrice > 100 ? 0 : 10
+ state.shippingPrice = addDecimal(shippingPrice)
+
+ const taxPrice = 0.15 * itemsPrice 
+ state.taxPrice = addDecimal(taxPrice)
+
+ const totalPrice = itemsPrice + shippingPrice + taxPrice;
+ state.totalPrice = addDecimal(taxPrice);
+
+ localStorage.setItem("cart", JSON.stringify(state))
+ return state
+}
