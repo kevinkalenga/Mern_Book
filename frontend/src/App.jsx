8c900e4e1b4163068b88ba7bdeaf04027037e2dc
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-18 02:00:15 +0200

    Implement check token expiration date

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,32 @@ import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css"
 import { ToastContainer } from "react-toastify";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "./slices/authSlice";
 
 function App() {
-   return (
+   const dispatch = useDispatch()
+  useEffect(() => {
+    const checkTokenExpiration = () => {
+       try {
+        const expirationTime = localStorage.getItem("expirationTime")
+        if(!expirationTime) return 
+
+        const isExpired = Date.now() > Number(expirationTime)
+
+        if(isExpired) {
+          dispatch(logout());
+        }
+       } catch (error) {
+         console.log("Error checking token expiration", error)
+       }
+    }
+    checkTokenExpiration();
+  }, [dispatch])
+  
+  
+  return (
     <>
     <ToastContainer />
       <Header />
