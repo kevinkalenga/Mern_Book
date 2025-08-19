4d26d8c6ba16f6e08d44e2a6eaf9eb39249f007e
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-20 01:51:49 +0200

    Order controller part 2

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -68,3 +68,78 @@ const getOrderById = asyncHandler(async (req, res) => {
     throw new Error("order not found");
   }
 });
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const { verified, value } = await verifyPaPalPayment(req.body.id);
+  if (!verified) throw new Error("Payment not verified");
+
+  const isNewTransaction = await checkIfNewTransaction(Order, req.body.id);
+  if (!isNewTransaction) throw new Error("Transaction has been used");
+
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    const paidCorrectAmount = order.totalPrice.toString() === value;
+    if (!paidCorrectAmount) throw new Error("Incorrect amount paid");
+
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    for (const item of order.orderItems) {
+      const product = await Product.findById(item.product);
+
+      if (!product) {
+        throw new Error(`Product ${item.product} not found`);
+      }
+
+      if (product.countInStock < item.qty) {
+        throw new Error(`Not enough stock for product ${product.name}`);
+      }
+
+      product.countInStock -= item.qty;
+      await product.save();
+    }
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+    const updatedOrder = await order.save();
+
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
+
+export {
+  addOrderItems,
+  getMyOrder,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+};
